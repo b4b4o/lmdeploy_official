6e4c4d1be286b17b3f5df52e9b7efdbcc4da1aca
b4b4o <zwbao@foxmail.com>
2024-04-11 16:28:07 +0800

    utils fix medusa_choices.info mc_sim_7b_63.

--- src/turbomind/models/medusa_plugin/tests/medusa_utils_test.cc
+++ src/turbomind/models/medusa_plugin/tests/medusa_utils_test.cc
@@ -3,14 +3,15 @@
 int main(){
     turbomind::MedusaPathTree tree;
     
-    std::string path_filename = "/workdir/lmdeploy/src/turbomind/models/medusa_plugin/medusa_choices.info";
-    std::string path_modelname = "mc_sim_7b_63";
+    // std::string path_filename = "/workdir/lmdeploy/src/turbomind/models/medusa_plugin/medusa_choices.info";
+    // std::string path_modelname = "mc_sim_7b_63";
 
-    turbomind::MedusaUtils utils(path_filename, path_modelname);
-    utils.displayPathTuples(utils.medusa_path_tuples_);
+    // turbomind::MedusaUtils utils(path_filename, path_modelname);
+    // utils.displayPathTuples(utils.medusa_path_tuples_);
 
-    // std::vector<std::vector<int>> path_tuples{{1, 1, 1}, {1, 2}, {2, 1, 1}, {2, 1, 2}, {2, 3}};
-    tree.insert(utils.medusa_path_tuples_);
+    std::vector<std::vector<int>> path_tuples{{1, 1, 1}, {1, 2}, {2, 1, 1}, {2, 1, 2}, {2, 3}};
+    // tree.insert(utils.medusa_path_tuples_);
+    tree.insert(path_tuples);
     tree.dbg();
 
     return 0;
